261a9e3604bb29be5b110118e5bc942efa02f86a
VanessaSarmiento <94455591+VanessaSarmiento@users.noreply.github.com>
2021-12-09 21:21:40 -0500

    Create netsize.sh
    Primer comando para llamar a la carpeta Saavedra2013

---
+++ Deber 1.10.3/netsize.sh
@@ -0,0 +1,7 @@
+1. Escriba un guión que tome uno de estos archivos y determine el número de filas (polinizadores)
+y columnas (plantas). Tenga en cuenta que las columnas están separadas por espacios y que hay un 
+espacio al final de cada línea. Tu guión debería regresar.
+
+Primer paso, se llamara la carpeta que contiene los datos:
+Saavedra2013, con el siguiente comando:
+$cd Saavedra2013
