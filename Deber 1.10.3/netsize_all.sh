5b3bb72fc3e2e681c0400c263e28567542c9275b
VanessaSarmiento <94455591+VanessaSarmiento@users.noreply.github.com>
2021-12-09 21:42:06 -0500

    Update netsize_all.sh
    Código para el conteo de filas y columnas de todo el contenido de todas las carpetas que tiene Saavedra2013

--- Deber 1.10.3/netsize_all.sh
+++ Deber 1.10.3/netsize_all.sh
@@ -1,7 +1,10 @@
 2. Escriba un guión que imprima el número de filas y
 columnas para cada red:
 
-Como primer paso la carpeta se llamará Saavedra2013
+Cómo primer paso la carpeta se llamará Saavedra2013
 con el comando:
 
 $cd Saavedra2013
+
+Cómo segundo paso  se utilizara el siguiente comando para el coneo de filas para todos archivos que contiene Saavedra2013:
+$for file $(ls *.txt); do wc -l $file; head -n1 $ file | grep -o ""| wc -l; doone
